ef1cabcee000d9795573dc7c0061343bb2f9286c
kliff99k <88235200+kliff99k@users.noreply.github.com>
2023-01-14 20:12:40 +0800

    nothing update
    none

--- main.py
+++ main.py
@@ -15,14 +15,16 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text('If you need help, you are in the wrong place...')
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    text = str(update.message).lower()
-    response = R.sample_responses(text)
+    text = (update.message)
+    # print(type(text))
+    print(str(text))
+    response = R.sample_responses(str(text))
 
     await update.message.reply_text(response)
-'''
+
 def error(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     print(f"Update {update} caused error {context.error}")
-'''
+
 def main():
     app = Application.builder().token(keys.API_KEY).build()
     
@@ -31,9 +33,9 @@ def main():
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("hello", hello))
 
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    app.add_handler(MessageHandler(filters.ALL, handle_message))
 
-    #app.add_error_handler(error)
+    app.add_error_handler(error)
     app.run_polling(0)
 
 main()
--- responses.py
+++ responses.py
@@ -2,15 +2,15 @@ from datetime import datetime
 
 def sample_responses(input_text):
     user_message = str(input_text)
-    print(user_message)
+    # print(user_message)
 
-    if user_message in ("hello", "hi", "sup"):
+    if "hi" or "hello" or "sup" in user_message:
         return "Hey! How's it going?" 
 
-    elif user_message in ("who are you", "who are you?"):
+    elif "who are you" or "who are you?" or "what are you" or "what are you?" in user_message:
         return "I am a bot to match you to a roomate that you will benefit from."
     
-    elif user_message in ("sleep"):
+    elif "sleep" in user_message:
         now = datetime.now()
         date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
         return str("Goodnight! It is currently " + date_time)
