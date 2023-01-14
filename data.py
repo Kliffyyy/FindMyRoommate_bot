6f01fbd13f246693b56589c8e94683833c4a2bc2
UserNotFound7 <ud3m0n3a@gmail.com>
2023-01-14 23:20:37 +0800

    do some questions

--- data.py
+++ data.py
@@ -1,12 +1,11 @@
 start = "Hi I am a bot to match you to a roomate that you will benefit from. Please enter your gender."
 help = "Please enter /start to begin the process.\nPlease enter /help to get help.\ncheck the description of this bot to find out what its capabilities are."
 qn = ["What is your age?", 
-"What is your major?" 
+"What is your major?", 
 "What are your hobbies?", 
 "What time do you sleep? (24 hour format)", 
 "What time do you wake up? (24 hour format)", 
 "Thank you for your time. I will match you with a roomate soon. Please wait...", 
 "I am matching you with a roomate. Please wait...", 
 "Here is your match list: \n" ]
-
 # db = 
